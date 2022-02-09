a7857bd0d0191ab5ff90c60cea8e09e1011116d3
Animagus <subhra.jit748@gmail.com>
2022-02-09 12:14:00 +0530

    Updates

--- final.py
+++ final.py
@@ -7,7 +7,8 @@ import array
 # First Window where you give the master key
 root = Tk()
 root.title('Password Manager')
-root.iconbitmap('C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
+root.iconbitmap(
+    'C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
 root.geometry('400x400')
 
 # global bg_label
@@ -19,15 +20,18 @@ root.geometry('400x400')
 master_pass = "admin"
 
 # Check the master key and open Home
+
+
 def checker():
     mk = master_key.get()
     if mk == master_pass:
         root.destroy()
-    
-    #Creation of the Home Window
+
+    # Creation of the Home Window
     main = Tk()
     main.title('Home')
-    main.iconbitmap('C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
+    main.iconbitmap(
+        'C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
     main.geometry('400x400')
 
     # Connect to MySQL
@@ -74,17 +78,17 @@ def checker():
         # declare arrays of the character that we need in out password
         DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                            'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
-                            'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                            'z']
+                             'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                             'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                             'z']
 
         UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                            'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
-                            'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-                            'Z']
+                             'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                             'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                             'Z']
 
         SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
-                '*', '(', ')', '<']
+                   '*', '(', ')', '<']
 
         COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
 
@@ -104,8 +108,8 @@ def checker():
 
         password = ""
         for x in temp_pass_list:
-                password = password + x
-                
+            password = password + x
+
         password_box.insert(0, password)
 
     # Printing all the passwords
@@ -126,7 +130,7 @@ def checker():
         site_box.delete(0, END)
         username_box.delete(0, END)
         password_box.delete(0, END)
-        
+
     # Adding an Entry to the Database
     def add_to_db():
         sql_command = "INSERT INTO pass (username, password, site) VALUES (%s, %s, %s)"
@@ -143,13 +147,14 @@ def checker():
         # Creation of Editing page
         edit = Tk()
         edit.title('Edit')
-        edit.iconbitmap('C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
+        edit.iconbitmap(
+            'C:/Users/subhr/OneDrive/Documents/Programs/Python/password_manager/lock.ico')
         edit.geometry('400x400')
 
         # Save changes to the Database
         def changes():
             sql_command = """UPDATE pass SET username = %s, password = %s, site = %s WHERE user_id = %s"""
-                
+
             user_name = username_box2.get()
             password = password_box2.get()
             site = site_box2.get()
@@ -164,47 +169,47 @@ def checker():
 
         sql2 = "SELECT * from pass WHERE user_id = %s"
         name2 = (edit_db_box.get(), )
-        print (name2)
+        print(name2)
         results = cur.execute(sql2, name2)
         results = cur.fetchall()
         print(results)
-        
+
         # Editing box Creation
         global site_box2
         site_box2 = Entry(edit)
-        site_box2.grid(row= 1, column= 1)
+        site_box2.grid(row=1, column=1)
         site_box2.insert(0, results[0][2])
 
         global username_box2
         username_box2 = Entry(edit)
-        username_box2.grid(row= 2, column= 1)
+        username_box2.grid(row=2, column=1)
         username_box2.insert(0, results[0][0])
 
         global password_box2
         password_box2 = Entry(edit)
-        password_box2.grid(row= 3, column= 1)
+        password_box2.grid(row=3, column=1)
         password_box2.insert(0, results[0][1])
 
         global id_box2
         id_box2 = Entry(edit)
-        id_box2.grid(row= 4, column= 1)
+        id_box2.grid(row=4, column=1)
         id_box2.insert(0, results[0][3])
 
         # Editing Label Creation
-        site_label = Label(edit, text= "Site Name")
-        site_label.grid(row= 1, column= 0)
+        site_label = Label(edit, text="Site Name")
+        site_label.grid(row=1, column=0)
 
-        username_label = Label(edit, text= "User name")
-        username_label.grid(row= 2, column= 0)
+        username_label = Label(edit, text="User name")
+        username_label.grid(row=2, column=0)
 
-        password_label = Label(edit, text= "password")
-        password_label.grid(row= 3, column= 0)
+        password_label = Label(edit, text="password")
+        password_label.grid(row=3, column=0)
 
-        id_label = Label(edit, text= "User ID")
-        id_label.grid(row= 4, column= 0)
+        id_label = Label(edit, text="User ID")
+        id_label.grid(row=4, column=0)
 
-        save_button = Button(edit, text= "Save all Changes", command= changes)
-        save_button.grid(row= 5, column= 0, columnspan= 2)
+        save_button = Button(edit, text="Save all Changes", command=changes)
+        save_button.grid(row=5, column=0, columnspan=2)
 
     # Fucntion to Delete n Entry in Database
     def delete_db():
@@ -222,62 +227,65 @@ def checker():
         # Entry Boxes for Data Entry
         global site_box
         site_box = Entry(main)
-        site_box.grid(row= 1, column= 1)
+        site_box.grid(row=1, column=1)
 
         global username_box
         username_box = Entry(main)
-        username_box.grid(row= 2, column= 1)
+        username_box.grid(row=2, column=1)
 
         global password_box
         password_box = Entry(main)
-        password_box.grid(row= 3, column= 1)
+        password_box.grid(row=3, column=1)
 
         global delete_from_db_box
         delete_from_db_box = Entry(main)
-        delete_from_db_box.grid(row= 6, column= 0)
+        delete_from_db_box.grid(row=6, column=0)
 
         global edit_db_box
         edit_db_box = Entry(main)
-        edit_db_box.grid(row= 5, column= 0)
+        edit_db_box.grid(row=5, column=0)
 
         # Labels for data Entry Boxes
-        site_label = Label(main, text= "Site Name")
-        site_label.grid(row= 1, column= 0)
+        site_label = Label(main, text="Site Name")
+        site_label.grid(row=1, column=0)
 
-        username_label = Label(main, text= "User name")
-        username_label.grid(row= 2, column= 0)
+        username_label = Label(main, text="User name")
+        username_label.grid(row=2, column=0)
 
-        password_label = Label(main, text= "password")
-        password_label.grid(row= 3, column= 0)
+        password_label = Label(main, text="password")
+        password_label.grid(row=3, column=0)
 
         # Buttons
-        save_button = Button(main, text= "Add", command= add_to_db)
-        save_button.grid(row= 4, column= 0)
+        save_button = Button(main, text="Add", command=add_to_db)
+        save_button.grid(row=4, column=0)
 
-        edit_button = Button(main, text= "Modify", command= edit_db)
-        edit_button.grid(row= 5, column= 1)
+        edit_button = Button(main, text="Modify", command=edit_db)
+        edit_button.grid(row=5, column=1)
 
-        delete_button = Button(main, text= "Delete", command= delete_db)
-        delete_button.grid(row= 6, column= 1)
+        delete_button = Button(main, text="Delete", command=delete_db)
+        delete_button.grid(row=6, column=1)
 
-        list_button = Button(main, text= "Show Entries", command= list_all)
-        list_button.grid(row= 4, column= 1)
+        list_button = Button(main, text="Show Entries", command=list_all)
+        list_button.grid(row=4, column=1)
 
-        generate_button = Button(main, text= "Generate Password", command= password_generator)
-        generate_button.grid(row= 3, column= 2)
-    
+        generate_button = Button(
+            main, text="Generate Password", command=password_generator)
+        generate_button.grid(row=3, column=2)
+
     # Main Button to Add Entries
-    add_button = Button(main, text= "+", font= ("Helvetica", 24), command= add_password)
-    add_button.grid(row= 0, column= 0)
+    add_button = Button(
+        main, text="+", font=("Helvetica", 24), command=add_password)
+    add_button.grid(row=0, column=0)
+
 
 # Entry box for Master Password
 global master_key
 
-master_key = Entry(root, show = '*')
+master_key = Entry(root, show='*')
 master_key.pack()
 
 # Button to check Master password
-master_key_btn = Button(root, text= "Log in", command= checker)
+master_key_btn = Button(root, text="Log in", command=checker)
 master_key_btn.pack()
 
 
